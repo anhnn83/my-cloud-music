82bc49345a7fa840c4870ca9d6b5f04f74af0afe
cronpostps <maychucuaai1@gmail.com>
2026-07-13 16:27:19 +0700

    no cookies for yt

--- src/modules/downloader-backend.js
+++ src/modules/downloader-backend.js
@@ -176,7 +176,7 @@ async function processSingleItem(item, index, existingFileNamesLower, formatId =
             embedThumbnail: true, addMetadata: true, 
             output: outputTemplate,
             noPlaylist: true,
-            // extractorArgs: 'youtube:player_client=android', 
+            extractorArgs: 'youtube:player_client=android.web',
             jsRuntimes: 'node',
             impersonate: 'chrome-110', 
             noCheckCertificates: true,
@@ -188,7 +188,7 @@ async function processSingleItem(item, index, existingFileNamesLower, formatId =
             maxSleepInterval: 15, // Nghỉ tối đa 15 giây (random khoảng 5-15s để giống người thật)
             // username: 'oauth2',
             // password: ''
-            cookies: '/app/cookies.txt'
+            // cookies: '/app/cookies.txt'
         };
 
         if (formatId) {
